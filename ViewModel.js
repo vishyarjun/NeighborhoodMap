ad9163002fcfa993aad0df5237d41572a439cb07
Arjun Viswanathan <arjunviswanathan@Arjuns-MacBook-Pro.local>
2017-09-22 12:30:43 +0530

    Implemented all the review comments

--- ViewModel.js
+++ ViewModel.js
@@ -1,111 +1,141 @@
 /**
-* @description This function is used to set a location when a name, id and location is passed.
-* @param {string} name - The name of the location
-* @param {integer} id - The id of the location
-* @param {Object} location - The lat long coordinate of the location
-*/
-
-function SetLocation(name,id, location) {
-	var self = this;
-	self.id = id;
-	self.name = name;
-	self.location = ko.observable(location);
+ * @description This function is used to set a location when a name, id and location is passed.
+ * @param {string} name - The name of the location
+ * @param {integer} id - The id of the location
+ * @param {Object} location - The lat long coordinate of the location
+ */
+function SetLocation(name, id, location) {
+    var self = this;
+    self.id = id;
+    self.name = name;
+    self.location = ko.observable(location);
 
 }
 
 /**
-* @description This function is used to create ko observable for all the location and form a list for all the locations
-*/
+ * @description This function is used to create ko observable for all the location and form a list for all the locations
+ */
 
 function NeighborhoodMapViewModel() {
 
-	var self = this;
-	self.searchval = ko.observable("");
-	self.location = [
-	{lat: -38.339, lng: 144.3104}, // Great Ocean Road
-	{lat: -37.7536, lng: 145.2218}, // Yarra River
-	{lat: -38.2678, lng: 144.6287}, // Queens Cliff
-	{lat: -37.9810, lng: 145.2150}, // Dandenong
-	{lat: -37.9000, lng: 144.6640}, //Weribee
-	{lat: -37.8225, lng: 144.9689}, //NGV
-	{lat: -37.907, lng: 145.3581}, // Puffing Billy
-	{lat: -37.8610, lng: 144.8850}, //Williamstown
-	{lat: -37.868, lng: 144.9767}, // Luna Park
-	{lat: -38.1499, lng: 144.3617} // Geelong
-		];
-self.neighborhoods = ko.observableArray([
-new SetLocation("Great Ocean Road",0, self.location[0]),
-new SetLocation("Yarra River",1, self.location[1]),
-new SetLocation("Queens Cliff",2, self.location[2]),
-new SetLocation("Dandenong",3, self.location[3]),
-new SetLocation("Weribee",4, self.location[4]),
-new SetLocation("National Gallery Of Victoria",5, self.location[5]),
-new SetLocation("Puffing Billy",6, self.location[6]),
-new SetLocation("Williamstown",7, self.location[7]),
-new SetLocation("Luna Park",8, self.location[8]),
-new SetLocation("Geelong",9, self.location[9]),
+    var self = this;
+    self.searchval = ko.observable("");
+    self.location = [{
+            lat: -38.339,
+            lng: 144.3104
+        }, // Great Ocean Road
+        {
+            lat: -37.7536,
+            lng: 145.2218
+        }, // Yarra River
+        {
+            lat: -38.2678,
+            lng: 144.6287
+        }, // Queens Cliff
+        {
+            lat: -37.9810,
+            lng: 145.2150
+        }, // Dandenong
+        {
+            lat: -37.9000,
+            lng: 144.6640
+        }, //Weribee
+        {
+            lat: -37.8225,
+            lng: 144.9689
+        }, //NGV
+        {
+            lat: -37.907,
+            lng: 145.3581
+        }, // Puffing Billy
+        {
+            lat: -37.8610,
+            lng: 144.8850
+        }, //Williamstown
+        {
+            lat: -37.868,
+            lng: 144.9767
+        }, // Luna Park
+        {
+            lat: -38.1499,
+            lng: 144.3617
+        } // Geelong
+    ];
+    self.neighborhoods = ko.observableArray([
+        new SetLocation("Great Ocean Road", 0, self.location[0]),
+        new SetLocation("Yarra River", 1, self.location[1]),
+        new SetLocation("Queens Cliff", 2, self.location[2]),
+        new SetLocation("Dandenong", 3, self.location[3]),
+        new SetLocation("Weribee", 4, self.location[4]),
+        new SetLocation("National Gallery Of Victoria", 5, self.location[5]),
+        new SetLocation("Puffing Billy", 6, self.location[6]),
+        new SetLocation("Williamstown", 7, self.location[7]),
+        new SetLocation("Luna Park", 8, self.location[8]),
+        new SetLocation("Geelong", 9, self.location[9]),
 
-]);
+    ]);
 
-self.setLists = function (locs) {
-	var count =0;
-	self.neighborhoods.removeAll();
-	for (var i = 0; i < locs.length; i++) {
- 	     	self.neighborhoods.push({
-            		name: self.copy()[locs[i]].name,
-            		id: self.copy()[locs[i]].id,
-            		location: self.copy()[locs[i]].location
-    });
-     }
-    for (var k = 0; k < markers.length; k++) {
-          markers[k].setMap(null);
+    self.setLists = function(locs) {
+        var count = 0;
+        self.neighborhoods.removeAll();
+        for (var i = 0; i < locs.length; i++) {
+            self.neighborhoods.push({
+                name: self.copy()[locs[i]].name,
+                id: self.copy()[locs[i]].id,
+                location: self.copy()[locs[i]].location
+            });
         }
-    for(var j=0;j < locs.length;j++)
-    {
-    	markers[locs[j]].setMap(map);
-    }
-};
-self.copy = ko.observableArray([
-new SetLocation("Great Ocean Road",0, self.location[0]),
-new SetLocation("Yarra River",1, self.location[1]),
-new SetLocation("Queens Cliff",2, self.location[2]),
-new SetLocation("Dandenong",3, self.location[3]),
-new SetLocation("Weribee",4, self.location[4]),
-new SetLocation("National Gallery Of Victoria",5, self.location[5]),
-new SetLocation("Puffing Billy",6, self.location[6]),
-new SetLocation("Williamstown",7, self.location[7]),
-new SetLocation("Luna Park",8, self.location[8]),
-new SetLocation("Geelong",9, self.location[9]),
+        for (var k = 0; k < markers.length; k++) {
+            markers[k].setMap(null);
+        }
+        for (var j = 0; j < locs.length; j++) {
+            markers[locs[j]].setMap(map);
+        }
+    };
+    self.copy = ko.observableArray([
+        new SetLocation("Great Ocean Road", 0, self.location[0]),
+        new SetLocation("Yarra River", 1, self.location[1]),
+        new SetLocation("Queens Cliff", 2, self.location[2]),
+        new SetLocation("Dandenong", 3, self.location[3]),
+        new SetLocation("Weribee", 4, self.location[4]),
+        new SetLocation("National Gallery Of Victoria", 5, self.location[5]),
+        new SetLocation("Puffing Billy", 6, self.location[6]),
+        new SetLocation("Williamstown", 7, self.location[7]),
+        new SetLocation("Luna Park", 8, self.location[8]),
+        new SetLocation("Geelong", 9, self.location[9]),
 
-]);
+    ]);
 
 
 }
 /**
-* @description This function is used to dynamically change the markers and list based on user input
-*/
-this.setMarkerAndList = function()
-	{
-	var locs = [];
-	var j = 0;
-	var sear = this.searchval();
-	for(var i=0;i<this.copy().length;i++)
-	{
-	var a = this.copy()[i].name;
-	if(a.match(sear))
-	{
-		locs[j]=this.copy()[i].id;
-		j++;
-	}
+ * @description This function is used to dynamically change the markers and list based on user input
+ */
+this.setMarkerAndList = function() {
+    map.setCenter(defaultLocation);
+    map.setZoom(9);
+    var locs = [];
+    var j = 0;
+    var sear = this.searchval();
+    for (var i = 0; i < this.copy().length; i++) {
+        var a = this.copy()[i].name;
+        if (a.match(sear)) {
+            locs[j] = this.copy()[i].id;
+            j++;
+        }
 
-}
-this.setLists(locs);
+    }
+    this.setLists(locs);
 };
-this.setInfo = function()
-	{
-		populateInfoWindow(markers[this.id]);
-	};
+this.setInfo = function() {
+    map.setZoom(11);
+    map.setCenter(markers[this.id].getPosition());
+    setWikiText(markers[this.id].title, markers[this.id]);
+    toggleBounce(markers[this.id]);
+};
 
-my = {viewModel: new NeighborhoodMapViewModel()};
+my = {
+    viewModel: new NeighborhoodMapViewModel()
+};
 ko.applyBindings(my.viewModel);
 var locations = my.viewModel.neighborhoods();
--- map.js
+++ map.js
@@ -2,243 +2,291 @@ var map;
 var markers = [];
 var marker;
 var options = [];
-var styles = [
-            {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
-            {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
-            {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
-            {
-              featureType: 'administrative.locality',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#d59563'}]
-            },
-            {
-              featureType: 'poi',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#d59563'}]
-            },
-            {
-              featureType: 'poi.park',
-              elementType: 'geometry',
-              stylers: [{color: '#263c3f'}]
-            },
-            {
-              featureType: 'poi.park',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#6b9a76'}]
-            },
-            {
-              featureType: 'road',
-              elementType: 'geometry',
-              stylers: [{color: '#38414e'}]
-            },
-            {
-              featureType: 'road',
-              elementType: 'geometry.stroke',
-              stylers: [{color: '#212a37'}]
-            },
-            {
-              featureType: 'road',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#9ca5b3'}]
-            },
-            {
-              featureType: 'road.highway',
-              elementType: 'geometry',
-              stylers: [{color: '#746855'}]
-            },
-            {
-              featureType: 'road.highway',
-              elementType: 'geometry.stroke',
-              stylers: [{color: '#1f2835'}]
-            },
-            {
-              featureType: 'road.highway',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#f3d19c'}]
-            },
-            {
-              featureType: 'transit',
-              elementType: 'geometry',
-              stylers: [{color: '#2f3948'}]
-            },
-            {
-              featureType: 'transit.station',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#d59563'}]
-            },
-            {
-              featureType: 'water',
-              elementType: 'geometry',
-              stylers: [{color: '#17263c'}]
-            },
-            {
-              featureType: 'water',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#515c6d'}]
-            },
-            {
-              featureType: 'water',
-              elementType: 'labels.text.stroke',
-              stylers: [{color: '#17263c'}]
-            }
-          ];
+var defaultLocation = {
+    lat: -38.1732,
+    lng: 144.8731
+};
+var styles = [{
+        elementType: 'geometry',
+        stylers: [{
+            color: '#242f3e'
+        }]
+    },
+    {
+        elementType: 'labels.text.stroke',
+        stylers: [{
+            color: '#242f3e'
+        }]
+    },
+    {
+        elementType: 'labels.text.fill',
+        stylers: [{
+            color: '#746855'
+        }]
+    },
+    {
+        featureType: 'administrative.locality',
+        elementType: 'labels.text.fill',
+        stylers: [{
+            color: '#d59563'
+        }]
+    },
+    {
+        featureType: 'poi',
+        elementType: 'labels.text.fill',
+        stylers: [{
+            color: '#d59563'
+        }]
+    },
+    {
+        featureType: 'poi.park',
+        elementType: 'geometry',
+        stylers: [{
+            color: '#263c3f'
+        }]
+    },
+    {
+        featureType: 'poi.park',
+        elementType: 'labels.text.fill',
+        stylers: [{
+            color: '#6b9a76'
+        }]
+    },
+    {
+        featureType: 'road',
+        elementType: 'geometry',
+        stylers: [{
+            color: '#38414e'
+        }]
+    },
+    {
+        featureType: 'road',
+        elementType: 'geometry.stroke',
+        stylers: [{
+            color: '#212a37'
+        }]
+    },
+    {
+        featureType: 'road',
+        elementType: 'labels.text.fill',
+        stylers: [{
+            color: '#9ca5b3'
+        }]
+    },
+    {
+        featureType: 'road.highway',
+        elementType: 'geometry',
+        stylers: [{
+            color: '#746855'
+        }]
+    },
+    {
+        featureType: 'road.highway',
+        elementType: 'geometry.stroke',
+        stylers: [{
+            color: '#1f2835'
+        }]
+    },
+    {
+        featureType: 'road.highway',
+        elementType: 'labels.text.fill',
+        stylers: [{
+            color: '#f3d19c'
+        }]
+    },
+    {
+        featureType: 'transit',
+        elementType: 'geometry',
+        stylers: [{
+            color: '#2f3948'
+        }]
+    },
+    {
+        featureType: 'transit.station',
+        elementType: 'labels.text.fill',
+        stylers: [{
+            color: '#d59563'
+        }]
+    },
+    {
+        featureType: 'water',
+        elementType: 'geometry',
+        stylers: [{
+            color: '#17263c'
+        }]
+    },
+    {
+        featureType: 'water',
+        elementType: 'labels.text.fill',
+        stylers: [{
+            color: '#515c6d'
+        }]
+    },
+    {
+        featureType: 'water',
+        elementType: 'labels.text.stroke',
+        stylers: [{
+            color: '#17263c'
+        }]
+    }
+];
+
+function googleError() {
+    document.getElementById("head").innerHTML = "<h1>Oops! Something went wrong. Please try again!</h1>";
+}
+
 
 /**
-* @description This function is used to create a bounce effect once the marker is clicked
-* @param {Object} marker - The marker which needs to be bounced.
-*/
+ * @description This function is used to create a bounce effect once the marker is clicked
+ * @param {Object} marker - The marker which needs to be bounced.
+ */
 
 function toggleBounce(marker) {
-        if (marker.getAnimation() !== null) {
-          marker.setAnimation(null);
-        } else {
-          for(var i=0;i<markers.length;i++)
-          {
+    if (marker.getAnimation() !== null) {
+        marker.setAnimation(null);
+    } else {
+        for (var i = 0; i < markers.length; i++) {
             markers[i].setAnimation(null);
-          }
-          marker.setAnimation(google.maps.Animation.BOUNCE);
         }
-      }
-/**
-* @description This function is used to initialize markers once the map is initialized
-*/
-function initMarkers()
-    {
-      var icon;
-    for(var i=0;i<locations.length;i++)
-        {
-
-  if(i%2==1)
-            {
-              icon = {
-
-    url: "gmark.png", // url
-    scaledSize: new google.maps.Size(30, 40)
-};
+        marker.setAnimation(google.maps.Animation.BOUNCE);
+    }
 }
-            else
-            {
-               icon = {
-    url: "graymark.png", // url
-    scaledSize: new google.maps.Size(30, 40)
-};
-            }
-            var position = locations[i].location();
-            var name = locations[i].name;
-            marker = new google.maps.Marker({
-                map: map,
-                position: position,
-                title: name,
-                icon: icon,
-                animation: google.maps.Animation.DROP
-
-            });
-            options.push(name);
-            markers.push(marker);
-            marker.addListener('click', function(){
-              toggleBounce(this);
-              populateInfoWindow(this);
-         });
-        }
-      }
 /**
-* @description This function is called as part of google maps url and used to initiate the map instance
-*/
-function initMap()
-  {
-    if(!map)
-    {
-    map = new google.maps.Map(document.getElementById('map'),{
-      center: {lat: -38.1732, lng: 144.8731},
-      styles: styles,
-      zoom: 9
+ * @description This function is used to initialize markers once the map is initialized
+ */
+function initMarkers() {
+    var icon;
+    for (var i = 0; i < locations.length; i++) {
+
+        if (i % 2 == 1) {
+            icon = {
+
+                url: "gmark.png", // url
+                scaledSize: new google.maps.Size(30, 40)
+            };
+        } else {
+            icon = {
+                url: "graymark.png", // url
+                scaledSize: new google.maps.Size(30, 40)
+            };
+        }
+        var position = locations[i].location();
+        var name = locations[i].name;
+        marker = new google.maps.Marker({
+            map: map,
+            position: position,
+            title: name,
+            icon: icon,
+            animation: google.maps.Animation.DROP
+
+        });
+        options.push(name);
+        markers.push(marker);
+        marker.addListener('click', function() {
+
+            map.setCenter(this.getPosition());
+            map.setZoom(11);
+            toggleBounce(this);
+            setWikiText(this.title, this);
+        });
+    }
+}
+/**
+ * @description This function is called as part of google maps url and used to initiate the map instance
+ */
+function initMap() {
+    if (!map) {
+        map = new google.maps.Map(document.getElementById('map'), {
+            center: defaultLocation,
+            styles: styles,
+            zoom: 9
+        });
+    }
+
+    largeInfoWindow = new google.maps.InfoWindow({
+        maxWidth: 320,
+        minHeight: 125
     });
-  }
-
-
-    largeInfoWindow = new google.maps.InfoWindow({maxWidth: 320,minHeight: 125} );
-      initMarkers();
-    $(window).resize(function () {
-    var h = $(window).height(),
-        offsetTop = 50; // Calculate the top offset
-    var w = $(window).width(),
-        offsetLeft = 100;
-    $('#map').css({'height':(h - offsetTop)});
-}).resize();
+    initMarkers();
+    $(window).resize(function() {
+        var h = $(window).height(),
+            offsetTop = 50; // Calculate the top offset
+        var w = $(window).width(),
+            offsetLeft = 100;
+        $('#map').css({
+            'height': (h - offsetTop)
+        });
+    }).resize();
 }
 
-
-
 /**
-* @description This function is used to display a info window once a marker is clicked/ when a list item is chosen
-* @param {Object} marker - The marker item for which the info window to be displayed.
-*/
-function populateInfoWindow(marker) {
-        // Check to make sure the largeInfoWindow is not already opened on this marker.
-        if (largeInfoWindow.marker != marker) {
-          // Clear the largeInfoWindow content to give the streetview time to load.
-          largeInfoWindow.setContent('');
-          largeInfoWindow.marker = marker;
-          // To Make sure the marker property is cleared if the largeInfoWindow is closed.
-          largeInfoWindow.addListener('closeclick', function() {
+ * @description Once the infoWindow is open, this function is called to display information about the place
+ * @param {string} title - The name of the location
+ */
+function setWikiText(title, marker) {
+    var res;
+    var qry = title.replace(/ /g, "%20");
+    var wikiURL = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + qry + "&format=json";
+    $.ajax({
+        async: false,
+        type: "GET",
+        url: wikiURL,
+        dataType: "jsonp",
+        success: function(data) {
+            populateInfoWindow(marker, data[2]);
+        }
+
+    }).fail(function(jqXHR, textStatus) {
+        populateInfoWindow(marker, "failed to load resource");
+
+    });
+
+}
+/**
+ * @description This function is used to display a info window once a marker is clicked/ when a list item is chosen
+ * @param {Object} marker - The marker item for which the info window to be displayed.
+ */
+function populateInfoWindow(marker, description) {
+    // Check to make sure the largeInfoWindow is not already opened on this marker.
+    if (largeInfoWindow.marker != marker) {
+        // Clear the largeInfoWindow content to give the streetview time to load.
+        largeInfoWindow.setContent('');
+        largeInfoWindow.marker = marker;
+        // To Make sure the marker property is cleared if the largeInfoWindow is closed.
+        largeInfoWindow.addListener('closeclick', function() {
             largeInfoWindow.marker = null;
-          });
-          var streetViewService = new google.maps.StreetViewService();
-          var radius = 50;
-          // In case the status is OK, which means the pano was found, compute the
-          // position of the streetview image, then calculate the heading, then get a
-          // panorama from that and set the options
-          function getStreetView(data, status) {
+        });
+        var streetViewService = new google.maps.StreetViewService();
+        var radius = 50;
+        // In case the status is OK, which means the pano was found, compute the
+        // position of the streetview image, then calculate the heading, then get a
+        // panorama from that and set the options
+        function getStreetView(data, status) {
             if (status == google.maps.StreetViewStatus.OK) {
-              var nearStreetViewLocation = data.location.latLng;
-              var heading = google.maps.geometry.spherical.computeHeading(
-                nearStreetViewLocation, marker.position);
-                largeInfoWindow.setContent(
-                  '<h3><center>' + marker.title + '</center></h3><div style="width: 320px; height: 100px" id="pano"></div>'+
-                  '<hr/><div id ="wikielem"></div>'
-                  );
+                var nearStreetViewLocation = data.location.latLng;
+                var heading = google.maps.geometry.spherical.computeHeading(
+                    nearStreetViewLocation, marker.position);
+                largeInfoWindow.setContent('<h3><center>' + marker.title + '</center></h3><div style="width: 320px; height: 100px" id="pano"></div>' +
+                    '<hr/><p>' + description + '</p>');
                 var panoramaOptions = {
-                  position: nearStreetViewLocation,
-                  pov: {
-                    heading: heading,
-                    pitch: 2
-                  }
+                    position: nearStreetViewLocation,
+                    pov: {
+                        heading: heading,
+                        pitch: 2
+                    }
                 };
-              var panorama = new google.maps.StreetViewPanorama(
-                document.getElementById('pano'), panoramaOptions);
-              setWikiText(marker.title);
+                var panorama = new google.maps.StreetViewPanorama(
+                    document.getElementById('pano'), panoramaOptions);
             } else {
-              largeInfoWindow.setContent('<div>' + marker.title + '</div>' +
-                '<div>No Street View Found</div>');
+                largeInfoWindow.setContent('<div>' + marker.title + '</div>' +
+                    '<div>No Street View Found</div><hr/><p>' + description + '</p>');
+
+
             }
-          }
-          streetViewService.getPanoramaByLocation(marker.position, radius, getStreetView);
-          // Open the largeInfoWindow on the correct marker.
-          largeInfoWindow.open(map, marker);
-
         }
-      }
-/**
-* @description Once the infoWindow is open, this function is called to display information about the place
-* @param {string} title - The name of the location
-*/
-function setWikiText(title)
-  {
-            var qry = title.replace(/ /g,"%20");
-              var wikiURL = "https://en.wikipedia.org/w/api.php?action=opensearch&search="+qry+"&format=json";
-              var wikiRequestTimeout = setTimeout(function(){document.getElementById('wikielem').innerHTML="Failed to get resources";},4000);
-              $.ajax({
-              type: "GET",
-              url: wikiURL,
-              dataType: "jsonp",
-              success: function (data) {
-                clearTimeout(wikiRequestTimeout);
-                document.getElementById('wikielem').innerHTML = data[2];
-                }
-    });
+        streetViewService.getPanoramaByLocation(marker.position, radius, getStreetView);
+        // Open the largeInfoWindow on the correct marker.
+        largeInfoWindow.open(map, marker);
 
-  }
-
-
-
-
+    }
+}
